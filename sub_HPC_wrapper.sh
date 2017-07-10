80734de7362bc2ebdba7e0183f9258da032b6abb
eelysium <tsem1@uci.edu>
2017-07-10 11:47:23 -0700

    Clean_071017

--- sub_HPC_wrapper.sh
+++ sub_HPC_wrapper.sh
@@ -16,21 +16,15 @@ module load gcc/4.8.2
 #One of the above modules is necessary to keep BNG running after 100 iterations or so. 
 #I don't know which module is necessary
 
-TAU="5000"
-#tau for both Voronoi and transition steps of the simulation
-TAU_NAME="5000_062917"
-#suffix of the output file
-SPECIES="8"
-#number of columns in the BNG file to read
-
-BINS="250"
-#number of voronoi regions
-LOOPS="1000"
-#number of WE loops for the transition matrix
-VORLOOPS="1"
-#number of WE loops for the voronoi regions
-REPS="200"
+TAU="5000"   #tau for both Voronoi and transition steps of the simulation
 FILE_ROOT="MISA"  #PREFIX for the output file
+TAU_NAME="5000_062917"   #suffix of the output file. Start with the tau number
+SPECIES="8" #number of columns starting from the first in the BNG file to read. i.e. the number of species
+
+BINS="250" #number of voronoi regions
+LOOPS="1000" #number of WE loops for the transition matrix
+VORLOOPS="1" #number of WE loops for the voronoi regions
+REPS="200" #Numbers of replicas
 BATCH="60" #number of BNG iterations in each job of the job-array (DON'T CHANGE)
 RSTART="1" #current iteration to restart from, set to 1 if new simulation
 OUTPUT="/dfs2/elread/rxn-share/WE_HPC_BATCH_BNG_CODES" #LOCATION WHERE YOU WANT TO SAVE THE OUTPUTS
@@ -40,19 +34,23 @@ TMP="/fast-scratch/${ME}/stage_name"  #TEMP LOCATION, MUST BE IN FAST-SCRATCH. M
 mkdir $TMP
 
 INFILEMAT="trans_NANOG_tau20_3SPEC_f50_062017.mat" #INPUT file for the voronoi, replicas and replica weights. Not necessary for initialization
-INFILEVOR="2" #current replica data to restart from
+INFILEVOR="2" #current replica data folder to restart from. Increments from [0-3]
 FULLRSTART="0" #FLAG FOR restarting transition matrix (1 for full restart, 0 for continuation)
 
 
 module load MATLAB/r2015a #loading matlab
 cp ${OUTPUT}/run_network ${TMP}/
 cp ${OUTPUT}/sub_BNG_batch_template.sh ${TMP}/
-cp ${OUTPUT}/sub_BNG_MISAEx_init.sh ${TMP}/
+cp ${OUTPUT}/sub_BNG_initialization.sh ${TMP}/  #only if you're initializing the simulation
+mkdir ${TMP}/BNG_net_files
+cp ${OUTPUT}/BNG_net_files/MISA_Ex_D.net ${TMP}/BNG_net_files/ #location of the BNG file
+BNG_netfile_name="BNG_net_files/MISA_Ex_D.net"
 
-mcc -m General_WE_trans_062617_MISA_init.m
-./General_WE_trans_062617_MISA_init $VORLOOPS $LOOPS $TMP $BINS $REPS $FILE_ROOT $TAU $TAU_NAME $OUTPUT $SPECIES $BATCH $RSTART
 
-#mcc -m General_WE_trans_062617_restart.m #for restarting
-#./General_WE_trans_062617_restart $VORLOOPS $LOOPS $TMP $BINS $REPS $FILE_ROOT $TAU $TAU_NAME $OUTPUT $INFILEMAT $INFILEVOR $SPECIES $BATCH $RSTART $FULLRSTART $EIGV
+mcc -m General_WE_transmatrixcalc_init_ver1.m
+./General_WE_transmatrixcalc_init_ver1 $VORLOOPS $LOOPS $TMP $BINS $REPS $FILE_ROOT $TAU $TAU_NAME $OUTPUT $SPECIES $BATCH $RSTART $BNG_netfile_name
+
+#mcc -m General_WE_transmatrixcalc_restart_ver1.m #for restarting
+#./General_WE_transmatrixcalc_restart_ver1 $VORLOOPS $LOOPS $TMP $BINS $REPS $FILE_ROOT $TAU $TAU_NAME $OUTPUT $INFILEMAT $INFILEVOR $SPECIES $BATCH $RSTART $FULLRSTART $EIGV
 
 
